a3c01381c97284a4c9b193429c053745ccbef777
Radium Coders <radiumcoders@gmail.com>
2025-12-18 18:22:05 +0530

    feat: Add dedicated pages for UI library, other, portfolio template, and tool categories.

--- app/catagory/portfolio-template/page.tsx
+++ app/catagory/portfolio-template/page.tsx
@@ -9,15 +9,17 @@ async function page() {
   return (
     <Container className="p-4 flex flex-col gap-4 h-fit border-b border-edge">
       <Header catagory={"portfolio-template"} seeMore={false} />
-      {data.map((item) => (
-        <div key={item.id} className="md:basis-1/2 lg:basis-1/3">
-          <Cards
-            title={item.title}
-            description={item.description}
-            githubUrl={item.githubUrl}
-          />
-        </div>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {data.map((item) => (
+          <div key={item.id}>
+            <Cards
+              title={item.title}
+              description={item.description}
+              githubUrl={item.githubUrl}
+            />
+          </div>
+        ))}
+      </div>
     </Container>
   );
 }
