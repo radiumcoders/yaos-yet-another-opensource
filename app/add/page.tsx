905325ab11ba4f599d5b72604aeda11aa20d6574
Radium Coders <radiumcoders@gmail.com>
2025-12-19 18:38:24 +0530

    feat: wrap AddDataForm in Suspense for improved loading state handling

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import AddDataForm from "@/components/actions/add-data";
 import Container from "@/components/core/container";
+import { Suspense } from "react";
 
 function page() {
   return (
     <main className="flex flex-col h-full">
       <Container className="h-full flex-1 flex items-center justify-center">
-        <AddDataForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <AddDataForm />
+        </Suspense>
       </Container>
     </main>
   );
