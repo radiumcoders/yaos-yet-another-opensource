73b9775c309c25e39d53fbe33be703c1620290df
Radium Coders <radiumcoders@gmail.com>
2025-12-20 12:33:09 +0530

    feat: remove authentication-related files and components

--- migrations/0000_mixed_kulan_gath.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "data" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"title" text NOT NULL,
-	"catagory" text NOT NULL,
-	"description" text NOT NULL,
-	"github_url" text NOT NULL,
-	"github_raw_url" text,
-	"registrie_name" text
-);
